73a5be4585db739457245f67bcbce94876e13cf7
배준형 <70988272+20190511@users.noreply.github.com>
2024-03-20 15:47:47 +0900

    Title: 코드트리 오마카세, Time: 63ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240320/코드트리 오마카세/codetree-omakase.cpp
+++ 240320/코드트리 오마카세/codetree-omakase.cpp
@@ -30,34 +30,34 @@ void eating(int timer) {
 		int man_pos = m->second.first, man_times = m->second.third;
 		deque<pair<int, int>>& q = rail[cur_name];
 
-		int maxCnt = (int)q.size(), cnt = 0;
-		for (deque<pair<int, int>>::iterator q_it = q.begin(); !q.empty() && cnt < maxCnt; cnt++) {
+		int maxCnt = (int)q.size();
+		deque<pair<int, int>> newQ;
+		for (int cnt = 0; cnt < maxCnt; cnt++) {
 			// 음식이 남자에게 도착할 때까지 필요한 시간을 계산한다.
-
+			pair<int, int> q_it = q.at(cnt);
 			int overtime, lasttime;
-			if (man_times < q_it->first) {
-				overtime = timer - q_it->first;
-				int new_man_pos = (man_pos - (q_it->first - man_times%rail_size));
+			if (man_times < q_it.first) {
+				overtime = timer - q_it.first;
+				int new_man_pos = (man_pos - (q_it.first % rail_size - man_times % rail_size));
 				while (new_man_pos < 0)
 					new_man_pos += rail_size;
-				lasttime = q_it->second > new_man_pos ? new_man_pos + rail_size - q_it->second : new_man_pos - q_it->second;
+				lasttime = q_it.second > new_man_pos ? new_man_pos + rail_size - q_it.second : new_man_pos - q_it.second;
 			}
 			else {
 				overtime = timer - man_times;
-				lasttime = q_it->second > man_pos ? man_pos + rail_size - q_it->second : man_pos - q_it->second;
+				lasttime = q_it.second > man_pos ? man_pos + rail_size - q_it.second : man_pos - q_it.second;
 			}
 
-			if (overtime >= lasttime) {
-				deque<pair<int, int>>::iterator dels = q_it;
-				q_it++;
-				q.erase(dels);
+			if (overtime < lasttime) {
+				newQ.push_back(q_it);
+			}
+			else {
 				m->second.second--;
 				if (!m->second.second)
 					delName.push_back(cur_name);
 			}
-			else
-				q_it++;
 		}
+		rail[cur_name] = newQ;
 	}
 
 	while (!delName.empty()) {
@@ -72,9 +72,11 @@ int main(void)
 {
 	int B;
 	//cin.tie(0)->sync_with_stdio(false);
-	//freopen("test.txt", "w", stdout);
+	//freopen("input.txt", "r", stdin);
 	cin >> rail_size >> B;
 	for (int k = 0; k < B; k++) {
+		if (k == 21326)
+			printf("Stop\n");
 		char name_char[32];
 		int cmd, t, x, cnt;
 		string name;
@@ -85,7 +87,7 @@ int main(void)
 			scanf("%d %d %s", &t, &x, name_char);
 			name = string(name_char);
 			map<string, deque<pair<int, int> > >::iterator tmp = rail.find(name);
-			int cur_pos = x - t%rail_size + 1;
+			int cur_pos = x - t % rail_size + 1;
 			while (cur_pos < 0)
 				cur_pos += rail_size;
 			cur_pos %= rail_size;
@@ -100,7 +102,7 @@ int main(void)
 		else if (cmd == 200) {
 			scanf("%d %d %s %d", &t, &x, name_char, &cnt);
 			name = string(name_char);
-			int cur_pos = x - t%rail_size + 1; //레일을 돌리고 난 후 올라가기 때문....
+			int cur_pos = x - t % rail_size + 1; //레일을 돌리고 난 후 올라가기 때문....
 			while (cur_pos < 0)
 				cur_pos += rail_size;
 			cur_pos %= rail_size;
