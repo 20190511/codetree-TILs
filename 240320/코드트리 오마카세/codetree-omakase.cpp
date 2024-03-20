644b5fc424a8e34c20796dadfb27a08d01ba3eba
배준형 <70988272+20190511@users.noreply.github.com>
2024-03-20 14:47:22 +0900

    Title: 코드트리 오마카세, Time: 1552ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240320/코드트리 오마카세/codetree-omakase.cpp
@@ -0,0 +1,120 @@
+//1130 ~ 시간안에 못 품/.
+
+/**
+* 로직이 꼬여버림 : 이미 놓는 시점을 통일해뒀는데 이걸 꼬아서 생각해서 재계산 재계산.... 
+* --> // 이미 포인터를 회전해서 배치를 했다는건, 사람을 배치한 순간의 그 원판을 동시대에 볼 수 있도록 설계해뒀기 때문.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <deque>
+#include <vector>
+#include <map>
+using namespace std;
+
+struct man_sub {
+	int first;
+	int second;
+	int third;
+};
+int timer = 1, rail_size;
+map<string, man_sub> man;
+map<string, deque<pair<int, int> > > rail; //회전, 고정
+
+// Eating 연산 수행
+void eating(int timer) {
+	for (map<string, man_sub>::iterator m = man.begin(); m != man.end(); m++) {
+		string cur_name = m->first;
+		int man_pos = m->second.first, man_times = m->second.third;
+		deque<pair<int, int>> &q = rail[cur_name];
+
+		int maxCnt = (int)q.size(), cnt = 0;
+		for (deque<pair<int, int>>::iterator q_it = q.begin(); !q.empty() && cnt < maxCnt; cnt++) {
+			// 음식이 남자에게 도착할 때까지 필요한 시간을 계산한다.
+			
+			int overtime, lasttime; 
+			if (man_times < q_it->first) {
+				overtime = timer - q_it->first;
+				int new_man_pos = (man_pos - (q_it->first - man_times));
+				while (new_man_pos < 0)
+					new_man_pos += rail_size;
+				lasttime = q_it->second > new_man_pos ? new_man_pos + rail_size - q_it->second : new_man_pos - q_it->second;
+			}
+			else {
+				overtime = timer - man_times;
+				lasttime = q_it->second > man_pos ? man_pos + rail_size - q_it->second : man_pos - q_it->second;
+			}
+			
+			if (overtime >= lasttime) {
+				deque<pair<int, int>>::iterator dels = q_it;
+				q_it++;
+				q.erase(dels);
+				m->second.second--;
+			}
+			else
+				q_it++;
+		}
+	}
+}
+
+
+int main(void)
+{
+	int B;
+	//cin.tie(0)->sync_with_stdio(false);
+	//freopen("test.txt", "w", stdout);
+	cin >> rail_size >> B;
+	for (int k = 0 ; k < B ; k++) {
+		char name_char[32];
+		int cmd, t, x, cnt;
+		string name;
+		scanf("%d", &cmd);
+
+		// 현재 시간 대비 
+		if (cmd == 100) {
+			scanf("%d %d %s", &t, &x, name_char);
+			name = string(name_char);
+			map<string, deque<pair<int,int> > >::iterator tmp = rail.find(name);
+			int cur_pos = x - t + 1;
+			while (cur_pos < 0)
+				cur_pos += rail_size; 
+			cur_pos %= rail_size;
+			if (tmp == rail.end()) {
+				rail[name] = deque<pair<int, int>>();
+				rail[name].push_back({ t, cur_pos });
+			}
+			else {
+				rail[name].push_back({ t, cur_pos });
+			}
+		}
+		else if (cmd == 200) {
+			scanf("%d %d %s %d", &t, &x, name_char, &cnt);
+			name = string(name_char);
+			int cur_pos = x - t + 1; //레일을 돌리고 난 후 올라가기 때문....
+			while (cur_pos < 0)
+				cur_pos += rail_size;
+			cur_pos %= rail_size;
+			man[name] = { cur_pos, cnt, t};
+		}
+		else if (cmd == 300) {
+			scanf("%d", &t);
+			// 사이에 적용된 계산들 수행
+			eating(t);
+			int total_cnt = 0;
+			for (map<string, deque<pair<int, int>>>::iterator it = rail.begin(); it != rail.end(); it++) {
+				total_cnt += (int)it->second.size();
+			}
+			
+			int man_cnt = 0;
+			for (map<string, man_sub>::iterator it = man.begin(); it != man.end(); it++) {
+				if (it->second.second)
+					man_cnt++;
+			}
+			printf("%d %d\n", man_cnt, total_cnt);
+		}
+	}
+
+
+	return 0;
+}
