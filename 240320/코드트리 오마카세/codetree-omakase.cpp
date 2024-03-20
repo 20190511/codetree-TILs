883e7028d4abd0921af84186b498e655640bf97e
배준형 <70988272+20190511@users.noreply.github.com>
2024-03-20 23:54:13 +0900

    Title: 코드트리 오마카세, Time: 827ms, Memory: 35MB, Status: Passed - CodeTree

--- 240320/코드트리 오마카세/codetree-omakase.cpp
+++ 240320/코드트리 오마카세/codetree-omakase.cpp
@@ -1,161 +1,142 @@
-#ifndef DEBUG
-// 재도전 ... 2036
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <unordered_map>
-#include <queue>
-#include <vector>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+
 using namespace std;
-struct detail {
-	int first;
-	int second;
-	int third;
+
+#define FOR(i,s,e) for(int i=s; i<e; ++i)
+#define Q_MAX 10
+
+int L, Q;
+struct CMD {
+    int cmd, x, t, n;
+    string name;
 };
+vector<CMD> commands;
+unordered_map<string, int> sitting_position; // 손님이 앉은 자리 매핑
+unordered_set<string> visitting_customer_names; // 방문한 손님 목록 저장 ( 재방문은 없으므로 중복이 없다 )
+unordered_map<string, int> visit_customer_time; // 손님이 방문한 시간대
+unordered_map<string, int> eat_cnt; // 손님이 먹어야할 초밥 갯수 매핑
+vector<CMD> v;
+int sushi_cnt; // 남은 초밥 갯수
+int customer_cnt; // 남은 손님 명수
 
-int rail_size, totalFoodCnt = 0, totalManCnt = 0;
-unordered_map <string, vector<pair<int, int>>> rail; //시간, 위치
-unordered_map <string, detail> man; //시간, 위치, 먹는 개수, 원래위치
-unordered_map <string, priority_queue<pair<int, int>>> prev_q;
-unordered_map <string, vector<pair<int, int>>> pq; //시간 위치
+void input() {
+    cin >> L >> Q;
+    FOR(i, 0, Q) {
+        int cmd = -1, t = -1, x = -1, n = -1;
+        string name = "";
 
-int calcLastTime(string name, int pos)
-{
-	int manPos = man[name].second;
-	int lt = manPos < pos ? manPos + rail_size - pos : manPos - pos;
-	return lt;
+        cin >> cmd;
+
+        if (cmd == 100) {
+            cin >> t >> x >> name;
+        }
+        else if (cmd == 200) {
+            cin >> t >> x >> name >> n;
+
+            visitting_customer_names.insert(name);
+            sitting_position[name] = x; // 오는 손님은 모두 다르다 ( 재방문 x )
+            visit_customer_time[name] = t; // 손님이 방문한 시각
+            eat_cnt[name] = n;
+        }
+        else if (cmd == 300) cin >> t;
+        commands.push_back({cmd,x,t,n,name});
+    }
 }
 
-void manSort(string name, int timer)
-{
-	auto rail_it = rail.find(name);
-	if (rail_it == rail.end()) {
-		pq[name] = vector<pair<int, int>>();
-		return;
-	}
+bool mcmp(CMD left, CMD right) {
+    if (left.t > right.t) return false;
+    if (left.t < right.t) return true;
 
+    if (left.cmd > right.cmd) return false;
+    if (left.cmd < right.cmd) return true;
 
-	detail& tmp = man[name];
-	int manTime = tmp.first, manPos = tmp.second;
-
-	vector<pair<int, int>>& dq = rail_it->second;
-	pq[name] = vector<pair<int, int>>();
-	prev_q[name] = priority_queue<pair<int, int>>();
-
-	while (!dq.empty()) {
-		int item_time = dq.back().first, item_pos = dq.back().second;
-		int item_cur_pos = (item_pos + (timer - item_time)) % rail_size;
-		int lasttime = calcLastTime(name, item_cur_pos);
-		prev_q[name].push({ -lasttime, dq.back().first }); //최소힙 정렬
-		dq.pop_back();
-	}
+    return false;
 }
 
+void sol() {
+    FOR(i, 0, commands.size()) {// 생성된 초밥이 언제 없어질지 기록
+        if (commands[i].cmd != 100) continue; 
+        CMD sushis = commands[i];
+        int meet_sushi_cutomer_time = 0; // 초밥과 손님이 만나게 할 수 있는 보정 시간
+        int eat_sushi_time = 0; //초밥을 먹는 시간
 
-void eatProcess(int curTime) {
-	deque<string> deleteQ;
-	for (auto man_it = prev_q.begin(); man_it != prev_q.end(); man_it++) {
-		priority_queue<pair<int, int>>& prev_q = man_it->second;
-		auto& man_obj = man[man_it->first];
-		while (!prev_q.empty()) {
-			int overtime = curTime - man_obj.first;
-			int lasttime = -prev_q.top().first;
-			if (overtime >= lasttime) {
-				prev_q.pop();
-				totalFoodCnt--;
-				man_obj.third--;
-				if (!man_obj.third)
-					deleteQ.push_back(man_it->first);
-			}
-			else
-				break;
-		}
-	}
+        if (sushis.t < visit_customer_time[sushis.name]) {
+            int now_sushi_position = sushis.x;
 
-	for (auto man_it = pq.begin(); man_it != pq.end(); man_it++) {
-		vector<pair<int, int>>& dq = man_it->second, newQ;
-		auto& man_obj = man[man_it->first];
-		while (!dq.empty()) {
-			int overtime = curTime - dq.back().second;
-			int lasttime = -dq.back().first;
-			if (overtime >= lasttime) {
-				totalFoodCnt--;
-				man_obj.third--;
-				if (!man_obj.third)
-					deleteQ.push_back(man_it->first);
-			}
-			else
-				newQ.push_back(dq.back());
-			dq.pop_back();
-		}
-		man_it->second = newQ;
-	}
+            int diff_t = visit_customer_time[sushis.name] - sushis.t;
 
-	while (!deleteQ.empty()) {
-		totalManCnt--;
-		rail.erase(deleteQ.back());
-		man.erase(deleteQ.back());
-		pq.erase(deleteQ.back());
-		prev_q.erase(deleteQ.back());
-		deleteQ.pop_back();
-	}
+            now_sushi_position = ((now_sushi_position + diff_t) % L);
 
+            if (sitting_position[sushis.name] >= now_sushi_position) {
+                meet_sushi_cutomer_time = sitting_position[sushis.name] - now_sushi_position;
+            }
+            else if (sitting_position[sushis.name] < now_sushi_position) {
+                meet_sushi_cutomer_time = (sitting_position[sushis.name] + L) - now_sushi_position;
+            }
+            eat_sushi_time = visit_customer_time[sushis.name] + meet_sushi_cutomer_time;
+        }
+        else {
+            int now_sushi_position = sushis.x;
+            if (sitting_position[sushis.name] >= now_sushi_position) {
+                meet_sushi_cutomer_time = sitting_position[sushis.name] - now_sushi_position;
+            }
+            else if (sitting_position[sushis.name] < now_sushi_position) {
+                meet_sushi_cutomer_time = (sitting_position[sushis.name] + L) - now_sushi_position;
+            }
+            eat_sushi_time = sushis.t + meet_sushi_cutomer_time; // 초밥을 먹을 수 있을 때 까지 걸리는 시간
+        }
+
+        // 초밥이 사라지는 시점을 저장
+        CMD tmp;
+        tmp.cmd = sushis.cmd + 1;
+        tmp.t = eat_sushi_time;
+        tmp.x = -1;
+        tmp.name = sushis.name;
+        tmp.n = -1;
+        v.push_back(tmp);
+    }
+
+    // 전체 명령과 초밥이 사라지는 시점에 대한 정보를 합침
+    commands.insert(commands.end(), v.begin(), v.end());
+
+    // t가 작을 수록, 명령이 작을수록 우선순위를 부여해 정렬함
+    // 명령 우선순위 (100 -> 101 -> 200 -> 300)
+    sort(commands.begin(), commands.end(), mcmp);
+
+    FOR(i, 0, commands.size()) {
+
+        if (commands[i].cmd == 100) {
+
+            ++sushi_cnt;
+        }
+        else if (commands[i].cmd == 101) {
+
+            --sushi_cnt;
+
+            string now_name = commands[i].name;
+            eat_cnt[now_name] = eat_cnt[now_name] - 1;
+            if (eat_cnt[now_name] <= 0) --customer_cnt; 
+        }
+        else if (commands[i].cmd == 200) {
+
+            ++customer_cnt;
+        }
+        else if (commands[i].cmd == 300) {
+
+            cout << customer_cnt << " " << sushi_cnt << '\n';
+        }
+    }
 }
 
-int main(void)
-{
-	//freopen("input.txt", "r", stdin);
-	//freopen("output.txt", "w", stdout);
-	int m;
-	cin.tie(0)->sync_with_stdio(false);
-	cin >> rail_size >> m;
-	while (m--) {
-		string name;
-		int cmd, t, x, n;
-		cin >> cmd >> t;
+int main() {
+    //freopen("input.txt", "r", stdin);
+    input();
+    sol();
 
-		if (cmd == 100) {
-			totalFoodCnt++;
-			cin >> x >> name;
-			auto pq_it = pq.find(name);
-			/*
-			int pos = x - t % rail_size + 1;
-			while (pos < 0) pos += rail_size;
-			*/
-			if (pq_it != pq.end()) {
-				int lt = man[name].second - x;
-				if (lt < 0) lt += rail_size;
-				pq_it->second.push_back({ -lt, t });
-				continue;
-			}
-
-			auto rail_it = rail.find(name);
-			if (rail_it == rail.end()) {
-				rail[name] = vector<pair<int, int>>();
-				rail[name].push_back({ t, x });
-			}
-			else
-				rail[name].push_back({ t,x });
-
-		}
-		else if (cmd == 200) {
-			totalManCnt++;
-			cin >> x >> name >> n;
-			/*
-			int pos = x - t % rail_size + 1;
-			while (pos < 0) pos += rail_size;
-			*/
-
-			man[name] = { t, x, n };
-			manSort(name, t);
-		}
-		else if (cmd == 300) {
-			eatProcess(t);
-			printf("%d %d\n", totalManCnt, totalFoodCnt);
-
-		}
-
-	}
-	return 0;
+    return 0;
 }
-#endif
