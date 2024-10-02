160f7312ea7c695bd192fb81b849149543010582
배준형 <70988272+20190511@users.noreply.github.com>
2024-10-02 13:22:47 +0900

    Title: 코드트리 투어, Time: 679ms, Memory: 16MB, Status: Passed - CodeTree

--- 241002/코드트리 투어/codetree-tour.cpp
+++ 241002/코드트리 투어/codetree-tour.cpp
@@ -1,241 +1,161 @@
-//코드트리 투어 11:30 ~ 13:30
-#define DEBUG false
-#define DEBUGS false
-
-#define INF 0x7FFFFFFF
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <queue>
-#include <unordered_map>
-#include <map>
-#include <set>
+#include <vector>
+#include <algorithm>
+
+#define INF 0x7fffffff // INT 최대값을 정의합니다
+#define MAX_N 2000 // 코드트리 랜드의 최대 도시 개수입니다
+#define MAX_ID 30005 // 여행상품 ID의 최대값입니다
+
 using namespace std;
-#define endl "\n"
 
-unordered_map<int, int> idxCvt; //idConverter
-int distArr[17][2001];
+int N, M; // 도시의 개수 N과 간선의 개수 M 입니다
+int A[MAX_N][MAX_N]; // 코드트리 랜드의 간선을 인접 행렬로 저장합니다
+int D[MAX_N]; // Dijkstra 알고리즘을 통해 시작도시 S부터 각 도시까지의 최단경로를 저장합니다
+bool isMade[MAX_ID]; // index에 해당하는 ID를 갖는 여행상품이 만들어진적 있는지 저장합니다.
+bool isCancel[MAX_ID]; // index에 해당하는 ID를 갖는 여행상품이 취소되었는지 저장합니다
+int S; // 여행 상품의 출발지 입니다
 
+// 여행 상품을 정의합니다
+struct Package {
+    int id; // 고유 식별자 ID
+    int revenue; // 매출
+    int dest; // 도착도시
+    int profit; // 여행사가 벌어들이는 수익
 
-struct item {
-	int pid;
-	int start;
-	int dest;
-	int re;
-
-	item(int pid=0, int dest=0, int re=0, int start=0) : pid(pid), re(re), dest(dest), start(start){}
-	const bool operator< (const item& b) const {
-		int sIdx = idxCvt[start];
-
-
-		int costA = distArr[sIdx][dest];
-		int costB = distArr[sIdx][b.dest];
-		
-		int optA = re - costA;
-		int optB = b.re - costB;
-
-		if (optA != optB)
-			return optA < optB;
-		return pid > b.pid;
-	}
+    struct Comparator {
+        bool operator()(const Package& a, const Package& b) const {
+            if (a.profit == b.profit) {
+                return a.id > b.id; // profit이 같으면 id가 작은 순으로
+            }
+            return a.profit < b.profit; // profit이 클수록 우선 순위 높게
+        }
+    };
 };
 
+// 최적의 여행 상품을 빠르게 찾아내기 위해 priority queue를 사용합니다
+priority_queue<Package, vector<Package>, Package::Comparator> pq;
 
-bool itemCheck[30001];
-item itemRecent[30001];
-
-
-struct cmd {
-	int cmd;
-	int id;
-	int revenue;
-	int dest;
-	int s;
-};
-
-map<int, int> distVec[2001];
-
-queue<cmd> cmdQ;
-
-int sArr[17]; //0번은 기본
-int totalS = 1;
-int N, M;
-int Q;
-
-
-priority_queue<item> pq[17];
-
-
-void dijkstr(int start, int idx) {
-#if DEBUG
-	cout << "dijk s = " << start<<endl;
-#endif
-	priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int, int>> > q;
-
-	for (int i = 0; i < N; i++) {
-		distArr[idx][i] = INF;
-	}
-	distArr[idx][start] = 0;
-
-	q.push({ 0, start });
-	while (!q.empty()) {
-		int n = q.top().second, dist = q.top().first;
-		q.pop();
-
-		for (auto it = distVec[n].begin(); it != distVec[n].end(); it++) {
-			
-			int next = it->first, nextDir = it->second;
-			int countDir = distArr[idx][n] + nextDir;
-			if (countDir < distArr[idx][next]) {
-				distArr[idx][next] = countDir;
-				q.push({ countDir, next });
-			}
-
-		}
-
-	}
-
+// dijkstra 알고리즘을 통해 시작도시 S에서 각 도시로 가는 최단거리를 구합니다.
+void dijkstra() {
+    bool visit[MAX_N];
+    for (int i=0;i<N;i++){
+        D[i] = INF;
+        visit[i] = false;
+    }
+    D[S] = 0;
+    for (int i=0; i<N-1;i++){
+        int v=0, minDist = INF;
+        for (int j=0;j<N;j++) {
+            if (!visit[j] && minDist > D[j]) {
+                v = j;
+                minDist = D[j];
+            }
+        }
+        visit[v] = true;
+        for (int j=0;j<N;j++){
+            if (!visit[j] && D[v] != INF && A[v][j] != INF && D[j] > D[v] + A[v][j]) {
+                D[j] = D[v] + A[v][j];
+            }
+        }
+    }
 }
 
-
-int curS = 1;
-
-
-int FindItem() {
-
-	int ret = -1;
-	item retItem(-1,-1,-1,-1);
-	while (!pq[curS].empty()) {
-		retItem = pq[curS].top();
-		pq[curS].pop();
-
-		ret = retItem.pid;
-		if (itemRecent[ret].re != retItem.re || itemRecent[ret].dest != retItem.dest) {
-			retItem.pid = -1;
-			continue;
-		}
-
-		if (itemCheck[retItem.pid])
-			break;
-
-		retItem.pid = -1;
-	}
-
-	if (retItem.pid == -1)
-		return -1;
-
-
-	int curDist = distArr[curS][retItem.dest];
-	int opt = retItem.re - curDist;
-
-	if (opt < 0 || curDist == INF) {
-		pq[curS].push(retItem);
-		return -1;
-	}
-
-	itemCheck[retItem.pid] = false;
-	return retItem.pid;
+// 코드트리랜드를 입력받고
+// 주어진 코드트리 랜드를 인접행렬에 저장합니다
+void buildLand() {
+    cin >> N >> M;
+    for (int i=0;i<N;i++){
+        for (int j=0;j<N;j++){
+            A[i][j] = INF;
+        }
+        // 출발지와 도착지가 같은 경우 거리는 0입니다
+        A[i][i] = 0;
+    }
+    for (int i=0;i<M;i++){
+        int u,v,w;
+        cin >> u >> v >> w;
+        // 양방향 간선에 대해 두 도시간 여러 간선이 주어질 수 있으므로 min 값으로 저장합니다
+        A[u][v] = min(A[u][v], w);
+        A[v][u] = min(A[v][u], w);
+    }
 }
+// 여행 상품을 추가합니다
+// 추가된 여행상품은 priority queue안에도 들어가야합니다.
+void addPackage(int id, int revenue, int dest) {
+    Package p;
+    isMade[id] = true;
+    p.id = id;
+    p.revenue = revenue;
+    p.dest = dest;
+    p.profit = p.revenue - D[p.dest];
 
-int main(void)
-{
-#if DEBUGS
-	freopen("q.txt", "r", stdin);
-#endif
+    pq.push(p);
+}
+// id에 해당하는 여행상품이 취소되었음을 기록합니다
+void cancelPackage(int id) {
+    // 만들어진적 있는 여행상품에 대해서만 취소할 수 있습니다.
+    if (isMade[id]) isCancel[id] = true;
+}
+// 최적의 여행상품을 판매합니다
+int sellPackage() {
+    while(!pq.empty()) {
+        Package p = pq.top();
+        // 최적이라고 생각한 여행 상품이 판매 불가능 하다면 while문을 빠져나가 -1을 반환합니다.
+        if (p.profit < 0)
+            break;
+        pq.pop();
+        if (!isCancel[p.id])
+            return p.id; // 해당 여행 상품이 취소되지 않았다면 정상 판매되므로 id를 반환합니다
+    }
+    return -1;
+}
+// 변경할 시작도시를 입력받고 변경됨에 따른 기존 여행상품 정보들을 수정합니다.
+void changeStart() {
+    cin >> S; // 변경된 시작도시를 반영합니다
+    vector<Package> packages;
+    dijkstra(); // 시작도시가 변경됨에 따라 각 도시로 가는 최단경로를 갱신합니다
+    // 기존의 여행상품들을 packages에 기록하며 priority queue에서 삭제합니다
+    while(!pq.empty()) {
+        packages.push_back(pq.top());
+        pq.pop();
+    }
+    for (Package p : packages) {
+        // 기존의 여행 상품들의 profit을 수정하여 새로이 priority queue에 넣습니다
+        addPackage(p.id, p.revenue, p.dest);
+    }
+}
+int main() {
+    int Q, id, revenue, dest;
+    cin >> Q;
+    // 총 Q개의 쿼리를 입력받습니다
+    while (Q--) {
+        int T;
+        cin >> T;
 
-	int cmd;
-	cin >> Q;
-	cin >> cmd >> N >> M;
-	for (int i = 0; i < M; i++) {
-		int v, u, w;
-		cin >> v >> u >> w;
-		if (v == u) continue;
+        // 쿼리의 종류에 따라 필요한 함수들을 호출하여 처리합니다
+        switch (T)
+        {
+        case 100:
+            buildLand();
+            dijkstra();
+            break;
+        case 200:
+            cin >> id >> revenue >> dest;
+            addPackage(id, revenue, dest);
+            break;
+        case 300:
+            cin >> id;
+            cancelPackage(id);
+            break;
+        case 400:
+            cout << sellPackage() << endl;
+            break;
+        case 500:
+            changeStart();
+            break;
+        }
+    }
 
-		int curWeight = distVec[v][u];
-		if (!curWeight || curWeight > w) {
-			distVec[v][u] = distVec[u][v] = w;
-		}
-	}
-
-	idxCvt[0] = 1;
-	sArr[1] = 0;
-
-	int id, re, dest, s;
-	for (int i = 0; i < Q-1; i++) {
-		cin >> cmd;
-
-		if (cmd == 200) {
-			cin >> id >> re>>dest;
-			cmdQ.push({ cmd,id,re, dest,0 });
-
-		}
-		else if (cmd == 300) {
-			cin >> id;
-			cmdQ.push({ cmd,id,0,0,0 });
-
-		}
-		else if (cmd == 400) {
-			cmdQ.push({ cmd, 0,0,0,0 });
-
-		}
-		else if (cmd == 500) {
-			cin >> s;
-			cmdQ.push({ cmd, 0,0,0,s });
-
-			if (!idxCvt[s]) {
-				totalS++;
-				idxCvt[s] = totalS;
-				sArr[totalS] = s;
-			}
-		}
-
-		//cmdQ.push_back
-	}
-
-	for (int i = 1; i <= totalS; i++) {
-		dijkstr(sArr[i], i);
-	}
-
-	while (!cmdQ.empty()) {
-		cmd = cmdQ.front().cmd, id = cmdQ.front().id, dest=cmdQ.front().dest, re = cmdQ.front().revenue, s=cmdQ.front().s;
-		cmdQ.pop();
-
-		if (cmd == 200) {
-#if DEBUG
-			cout << "cmd=200, id=" << id << ", revenue=" << re << ", dest=" << dest << endl;
-#endif
-
-			itemRecent[id].pid = id;
-			itemRecent[id].dest = dest;
-			itemRecent[id].re = re;
-			itemCheck[id] = true;
-
-
-			for(int i = 1; i <= totalS; i++) {
-				itemRecent[id].start = sArr[i];
-				pq[i].push(itemRecent[id]);
-			}
-		}
-		else if (cmd == 300) {
-#if DEBUG
-			cout << "cmd=300, id=" << id << endl;
-#endif
-
-			itemCheck[id] = false;
-		}
-		else if (cmd == 400) {
-#if DEBUG
-			cout << "cmd=400" << endl;
-#endif
-			cout << FindItem() << endl;
-		}
-		else if (cmd == 500) {
-#if DEBUG
-			cout << "cmd=500, s=" << s << endl;
-#endif
-			curS = idxCvt[s];
-		}
-	}
-
-
-	return 0;
+    return 0;
 }
